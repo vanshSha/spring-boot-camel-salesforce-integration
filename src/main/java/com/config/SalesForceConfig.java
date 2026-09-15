1ea64dc870741a943638a6cdb09de4a57e2eb7a3
vanshSha <135006199+TFPankajGoyal@users.noreply.github.com>
2026-09-15 15:20:56 +0530

    Add Kubernetes deployment and externalize Salesforce configuration

--- src/main/java/com/config/SalesForceConfig.java
+++ src/main/java/com/config/SalesForceConfig.java
@@ -1,5 +1,6 @@
 package com.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,10 +8,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class SalesForceConfig {
 
-    @Bean
-    public WebClient webClient() {
-        return WebClient.builder()
-                .baseUrl("https://orgfarm-c796dc6e9a-dev-ed.develop.my.salesforce.com")
-                .build();
-    }
+    @Value("${salesforce.base-url}")
+    private String baseUrl;
+
+@Bean
+public WebClient webClient() {
+    return WebClient.builder()
+            .baseUrl(baseUrl)
+            .build();
+}
 }
